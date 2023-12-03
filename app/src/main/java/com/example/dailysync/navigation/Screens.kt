e777590dc48aadb3cab944aae891d753b55daa41
Rddrigo Cabrito <rodri.cabrito@gmail.com>
2023-12-03 15:40:03 +0000

    + Report/Community/Profile routes

--- app/src/main/java/com/example/dailysync/navigation/Screens.kt
+++ app/src/main/java/com/example/dailysync/navigation/Screens.kt
@@ -4,6 +4,9 @@ sealed class Screens(val route: String) {
     object Login : Screens("login_screen")
     object SignUp : Screens("signup_screen")
     object Home : Screens("home_screen")
+    object Reports : Screens("reports_screen")
+    object Community : Screens("community_screen")
+    object Profile : Screens("profile_screen")
 
     object Exercise : Screens("exercise/{result}")
     object Read : Screens("read_screen")
