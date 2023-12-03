9981d83b156271ead4b93322fb4a2fd08b158363
Rddrigo Cabrito <rodri.cabrito@gmail.com>
2023-12-03 18:18:56 +0000

    + screens for exercise (Start/During/Save)

--- app/src/main/java/com/example/dailysync/navigation/Screens.kt
+++ app/src/main/java/com/example/dailysync/navigation/Screens.kt
@@ -1,12 +1,21 @@
 package com.example.dailysync.navigation
 
 sealed class Screens(val route: String) {
+
+    // Start
     object Login : Screens("login_screen")
     object SignUp : Screens("signup_screen")
+
+    // Footer
     object Home : Screens("home_screen")
     object Reports : Screens("reports_screen")
     object Community : Screens("community_screen")
     object Profile : Screens("profile_screen")
+
+    // Exercise
+    object StartExercise : Screens("start_exercise_screen/{category}")
+    object DuringExercise : Screens("during_exercise_screen/{category}")
+    object SaveExercise : Screens("save_exercise_screen/{category}")
 
     object Exercise : Screens("exercise/{result}")
     object Read : Screens("read_screen")
