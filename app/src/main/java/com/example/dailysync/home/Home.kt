3cf206042231f09011aef1b907857091349a31c7
Rddrigo Cabrito <rodri.cabrito@gmail.com>
2023-12-03 15:41:07 +0000

    implementation of home options for each category

--- app/src/main/java/com/example/dailysync/home/Home.kt
+++ app/src/main/java/com/example/dailysync/home/Home.kt
@@ -8,12 +8,17 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,6 +31,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.dailysync.navigation.Screens
 import com.example.dailysync.ui.theme.DailySyncTheme
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -35,6 +41,10 @@ import com.google.firebase.ktx.Firebase
 fun Home(navController: NavController, auth: FirebaseAuth) {
 
     //Text("Welcome ${auth.currentUser?.email}")
+
+    var showExerciseOptions by remember { mutableStateOf(false) }
+    var showSleepOptions by remember { mutableStateOf(false) }
+    var showReadOptions by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
@@ -55,42 +65,213 @@ fun Home(navController: NavController, auth: FirebaseAuth) {
         Spacer(modifier = Modifier.height(20.dp))
 
         // body
-        Button(
-            onClick = { /* Handle button click */ },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 16.dp, start = 10.dp, end = 10.dp)
-                .height(175.dp)
-                .background(Color.Gray, shape = RoundedCornerShape(8.dp)),
 
-        ) {
-            Text("Exercise")
+        // Exercise
+        if (!showExerciseOptions) {
+            Button(
+                onClick = {
+                    showExerciseOptions = true
+                    showSleepOptions = false
+                    showReadOptions = false
+                          },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, start = 10.dp, end = 10.dp)
+                    .height(175.dp)
+                    .background(Color.Gray, shape = RoundedCornerShape(8.dp)),
+            ) {
+                Text("Exercise")
+            }
+        }
+
+        // exercise options appear when Exercise button is clicked
+        if (showExerciseOptions) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, start = 10.dp, end = 10.dp)
+                    .background(Color.Green, shape = RoundedCornerShape(8.dp))
+                    .height(175.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Green, shape = RoundedCornerShape(8.dp))
+                ) {
+
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                    // Button 1
+                    Button(
+                        onClick = { /* Handle button click */ },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                    ) {
+                        Text("Walk")
+                    }
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    // Button 2
+                    Button(
+                        onClick = { /* Handle button click */ },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                    ) {
+                        Text("Run")
+                    }
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    // Button 3
+                    Button(
+                        onClick = { /* Handle button click */ },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                    ) {
+                        Text("Cycle")
+                    }
+                }
+            }
         }
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(
-            onClick = { /* Handle button click */ },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 16.dp, start = 10.dp, end = 10.dp)
-                .height(175.dp)
-                .background(Color.Gray, shape = RoundedCornerShape(8.dp))
-        ) {
-            Text("Sleep")
+        // Sleep
+        if (!showSleepOptions) {
+            Button(
+                onClick = {
+                    showExerciseOptions = false
+                    showSleepOptions = true
+                    showReadOptions = false
+                          },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, start = 10.dp, end = 10.dp)
+                    .height(175.dp)
+                    .background(Color.Gray, shape = RoundedCornerShape(8.dp))
+            ) {
+                Text("Sleep")
+            }
+        }
+
+        // sleep options appear when Sleep button is clicked
+        if (showSleepOptions) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, start = 10.dp, end = 10.dp)
+                    .background(Color.Green, shape = RoundedCornerShape(8.dp))
+                    .height(175.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Green, shape = RoundedCornerShape(8.dp))
+                ) {
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    // Button 1
+                    Button(
+                        onClick = { /* Handle button click */ },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(75.dp)
+                    ) {
+                        Text("Register Today's Sleep")
+                    }
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    // Button 2
+                    Button(
+                        onClick = { /* Handle button click */ },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(75.dp)
+                    ) {
+                        Text("Define Sleep Schedule")
+                    }
+                }
+            }
         }
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(
-            onClick = { /* Handle button click */ },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 16.dp, start = 10.dp, end = 10.dp)
-                .height(175.dp)
-                .background(Color.Gray, shape = RoundedCornerShape(8.dp))
-        ) {
-            Text("Read")
+        // Read
+        if (!showReadOptions) {
+            Button(
+                onClick = {
+                    showExerciseOptions = false
+                    showSleepOptions = false
+                    showReadOptions = true
+                          },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, start = 10.dp, end = 10.dp)
+                    .height(175.dp)
+                    .background(Color.Gray, shape = RoundedCornerShape(8.dp))
+            ) {
+                Text("Read")
+            }
+        }
+
+        // read options appear when Read button is clicked
+        if (showReadOptions) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, start = 10.dp, end = 10.dp)
+                    .background(Color.Green, shape = RoundedCornerShape(8.dp))
+                    .height(175.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Green, shape = RoundedCornerShape(8.dp))
+                ) {
+
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                    // Button 1
+                    Button(
+                        onClick = { /* Handle button click */ },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                    ) {
+                        Text("Find Books")
+                    }
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    // Button 2
+                    Button(
+                        onClick = { /* Handle button click */ },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                    ) {
+                        Text("Currently Reading")
+                    }
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    // Button 3
+                    Button(
+                        onClick = { /* Handle button click */ },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                    ) {
+                        Text("Wishlist")
+                    }
+                }
+            }
         }
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -102,7 +283,7 @@ fun Home(navController: NavController, auth: FirebaseAuth) {
                 .fillMaxWidth()
         ) {
             Button(
-                onClick = { /*TODO*/ },
+                onClick = { navController.navigate(Screens.Home.route)},
                 modifier = Modifier
                     .weight(1f)
                     .height(80.dp)
@@ -112,7 +293,7 @@ fun Home(navController: NavController, auth: FirebaseAuth) {
             }
 
             Button(
-                onClick = { /*TODO*/ },
+                onClick = { navController.navigate(Screens.Reports.route) },
                 modifier = Modifier
                     .weight(1f)
                     .height(80.dp)
@@ -122,7 +303,7 @@ fun Home(navController: NavController, auth: FirebaseAuth) {
             }
 
             Button(
-                onClick = { /*TODO*/ },
+                onClick = { navController.navigate(Screens.Community.route) },
                 modifier = Modifier
                     .weight(1f)
                     .height(80.dp)
@@ -135,7 +316,7 @@ fun Home(navController: NavController, auth: FirebaseAuth) {
             }
 
             Button(
-                onClick = { /*TODO*/ },
+                onClick = { navController.navigate(Screens.Profile.route) },
                 modifier = Modifier
                     .weight(1f)
                     .height(80.dp)
@@ -145,28 +326,4 @@ fun Home(navController: NavController, auth: FirebaseAuth) {
             }
         }
     }
-
-    /*
-    Spacer(modifier = Modifier.height(16.dp))
-
-    Button(
-        onClick = {},
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(100.dp)
-    ) {
-        Text("Sleep")
-    }
-
-    Spacer(modifier = Modifier.height(16.dp))
-
-    Button(
-        onClick = {},
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(100.dp)
-    ) {
-        Text("Read")
-    }
-    */
 }
