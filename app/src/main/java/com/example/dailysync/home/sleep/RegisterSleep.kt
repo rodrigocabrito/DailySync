e9e6b1926ed09f4c5797fff1d5939c2e4b7f26d1
Rddrigo Cabrito <rodri.cabrito@gmail.com>
2023-12-04 01:14:05 +0000

    main fun declaration

--- app/src/main/java/com/example/dailysync/home/sleep/RegisterSleep.kt
+++ app/src/main/java/com/example/dailysync/home/sleep/RegisterSleep.kt
@@ -1,2 +1,10 @@
 package com.example.dailysync.home.sleep
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
+
+@Composable
+fun RegisterSleep(navController: NavController, auth: FirebaseAuth) {
+
+}
