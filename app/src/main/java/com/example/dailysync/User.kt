1f5b110c307e95364b2d6a79f56f35e5e8b497fb
joaocosta <fc54482@alunos.fc.ul.pt>
2023-12-20 16:20:18 +0000

    Save and represent current user name in home screen

--- app/src/main/java/com/example/dailysync/User.kt
+++ app/src/main/java/com/example/dailysync/User.kt
@@ -1,6 +1,7 @@
 package com.example.dailysync
 
 data class User(                    // TODO ADD FIELDS (STATS, PREFERENCES, ...)
+    var name: String = "",
     val email: String = "",
     val password: String = ""
 )
