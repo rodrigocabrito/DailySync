acf97bd12e63e5d115b48c33c1f95699c6c86844
Rddrigo Cabrito <rodri.cabrito@gmail.com>
2023-12-23 21:53:50 +0000

    adding footer menu

--- app/src/main/java/com/example/dailysync/home/sleep/RegisterSleep.kt
+++ app/src/main/java/com/example/dailysync/home/sleep/RegisterSleep.kt
@@ -3,6 +3,7 @@ package com.example.dailysync.home.sleep
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -34,11 +36,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.dailysync.R
 import com.example.dailysync.Sleep
 import com.example.dailysync.navigation.Screens
 import com.google.firebase.auth.FirebaseAuth
@@ -288,7 +292,7 @@ fun RegisterSleep(navController: NavController, auth: FirebaseAuth) {
             }
         }
 
-        Spacer(modifier = Modifier.height(40.dp))
+        Spacer(modifier = Modifier.height(20.dp))
 
         // awake time
 
@@ -499,6 +503,131 @@ fun RegisterSleep(navController: NavController, auth: FirebaseAuth) {
                     }
                 }
             )
+        }
+
+        Spacer(modifier = Modifier.height(6.dp))
+
+        // footer
+        Row(
+            modifier = Modifier
+                .padding(top = 10.dp)
+                .fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(80.dp)
+                    .background(Color(android.graphics.Color.parseColor("#2C8CBC")))
+                    .clickable {
+                        navController.navigate(Screens.Home.route)
+                    }
+                    .border(1.dp, Color.Black)
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center, // Center vertically
+                    horizontalAlignment = Alignment.CenterHorizontally // Center horizontally
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.home_icon),
+                        contentDescription = null,
+                        tint = Color.Black,
+                        modifier = Modifier
+                            .size(35.dp)
+                            .align(Alignment.CenterHorizontally)
+                    )
+                    Spacer(modifier = Modifier.height(6.dp))
+                    Text( "Home")
+
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(80.dp)
+                    .background(Color(android.graphics.Color.parseColor("#A2D6F0")))
+                    .clickable {
+                        navController.navigate(Screens.Reports.route)
+                    }
+                    .border(1.dp, Color.Black)
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center, // Center vertically
+                    horizontalAlignment = Alignment.CenterHorizontally // Center horizontally
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.report_icon),
+                        contentDescription = null,
+                        tint = Color.Black,
+                        modifier = Modifier
+                            .size(35.dp)
+                            .align(Alignment.CenterHorizontally)
+                            .padding(top = 10.dp)
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text( text ="Report")
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(80.dp)
+                    .background(Color(android.graphics.Color.parseColor("#A2D6F0")))
+                    .clickable {
+                        navController.navigate(Screens.Community.route)
+                    }
+                    .border(1.dp, Color.Black)
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center, // Center vertically
+                    horizontalAlignment = Alignment.CenterHorizontally // Center horizontally
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.community_icon),
+                        contentDescription = null,
+                        tint = Color.Black,
+                        modifier = Modifier
+                            .size(45.dp)
+                            .align(Alignment.CenterHorizontally)
+                    )
+                    Text( text = "Community",
+                        fontSize = 14.sp)
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(80.dp)
+                    .background(Color(android.graphics.Color.parseColor("#A2D6F0")))
+                    .clickable {
+                        navController.navigate(Screens.Profile.route)
+                    }
+                    .border(1.dp, Color.Black)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 10.dp),
+                    verticalArrangement = Arrangement.Center, // Center vertically
+                    horizontalAlignment = Alignment.CenterHorizontally // Center horizontally
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.profile_icon),
+                        contentDescription = null,
+                        tint = Color.Black,
+                        modifier = Modifier
+                            .size(30.dp)
+                            .align(Alignment.CenterHorizontally)
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+                    Text( "Profile")
+                }
+            }
         }
     }
 }
