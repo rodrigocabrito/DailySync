061a189ce2283a3b36af4b433e7744cb9acb9da1
Rddrigo Cabrito <rodri.cabrito@gmail.com>
2024-01-17 18:19:00 +0000

    adding date to the exercise

--- app/src/main/java/com/example/dailysync/Exercise.kt
+++ app/src/main/java/com/example/dailysync/Exercise.kt
@@ -1,10 +1,13 @@
 package com.example.dailysync
 
+import java.time.Instant
+
 data class Exercise(
     val name: String,
     val description: String = "",
     val time: Long,
     val averagePace: Float,
     val distance: Float,
-    val image: Boolean
+    val image: Boolean,
+    val date: Instant
 )
