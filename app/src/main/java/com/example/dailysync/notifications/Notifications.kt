874d99dbc0889e57d6d74b94e24de0002f0e512d
Rddrigo Cabrito <rodri.cabrito@gmail.com>
2024-01-17 17:47:10 +0000

    adding footer menu

--- app/src/main/java/com/example/dailysync/notifications/Notifications.kt
+++ app/src/main/java/com/example/dailysync/notifications/Notifications.kt
@@ -1,3 +1,158 @@
 package com.example.dailysync.notifications
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.dailysync.R
+import com.example.dailysync.navigation.Screens
+import com.google.firebase.auth.FirebaseAuth
+
 // TODO ALL
+
+@Composable
+fun Notifications(navController: NavController, auth: FirebaseAuth) {
+
+    // footer
+    Row(
+        modifier = Modifier
+            .padding(top = 10.dp)
+            .fillMaxWidth()
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .height(80.dp)
+                .background(Color(android.graphics.Color.parseColor("#A2D6F0")))
+                .clickable {
+                    navController.navigate(Screens.Home.route)
+                }
+                .border(1.dp, Color.Black)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center, // Center vertically
+                horizontalAlignment = Alignment.CenterHorizontally // Center horizontally
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.home_icon),
+                    contentDescription = null,
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .size(35.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
+                Spacer(modifier = Modifier.height(6.dp))
+                Text( "Home")
+
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .height(80.dp)
+                .background(Color(android.graphics.Color.parseColor("#A2D6F0")))
+                .clickable {
+                    navController.navigate(Screens.Reports.route)
+                }
+                .border(1.dp, Color.Black)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center, // Center vertically
+                horizontalAlignment = Alignment.CenterHorizontally // Center horizontally
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.report_icon),
+                    contentDescription = null,
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .size(35.dp)
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top = 10.dp)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text( text ="Report")
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .height(80.dp)
+                .background(Color(android.graphics.Color.parseColor("#A2D6F0")))
+                .clickable {
+                    navController.navigate(Screens.Community.route)
+                }
+                .border(1.dp, Color.Black)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center, // Center vertically
+                horizontalAlignment = Alignment.CenterHorizontally // Center horizontally
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.community_icon),
+                    contentDescription = null,
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .size(45.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
+                Text( text = "Community",
+                    fontSize = 14.sp)
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .height(80.dp)
+                .background(Color(android.graphics.Color.parseColor("#A2D6F0")))
+                .clickable {
+                    navController.navigate(Screens.Profile.route)
+                }
+                .border(1.dp, Color.Black)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 10.dp),
+                verticalArrangement = Arrangement.Center, // Center vertically
+                horizontalAlignment = Alignment.CenterHorizontally // Center horizontally
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.profile_icon),
+                    contentDescription = null,
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .size(30.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+                Text( "Profile")
+            }
+        }
+    }
+
+}
