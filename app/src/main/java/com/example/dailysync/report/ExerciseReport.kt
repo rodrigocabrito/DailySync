02b4cc9556abd5b896e669a163c4692f797e2643
Rddrigo Cabrito <rodri.cabrito@gmail.com>
2024-01-18 02:59:02 +0000

    initial skeleton

--- app/src/main/java/com/example/dailysync/report/ExerciseReport.kt
+++ app/src/main/java/com/example/dailysync/report/ExerciseReport.kt
@@ -1,3 +1,12 @@
 package com.example.dailysync.report
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
+
 // TODO ALL
+
+@Composable
+fun ExerciseReport(navController: NavController, auth: FirebaseAuth) {
+
+}
--- app/src/main/java/com/example/dailysync/report/SleepReport.kt
+++ app/src/main/java/com/example/dailysync/report/SleepReport.kt
@@ -1,3 +1,12 @@
 package com.example.dailysync.report
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
+
 // TODO ALL
+
+@Composable
+fun SleepReport(navController: NavController, auth: FirebaseAuth) {
+
+}
