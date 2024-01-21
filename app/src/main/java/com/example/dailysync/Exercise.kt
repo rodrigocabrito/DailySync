c6fb88cf1260b3343c9c69759578dc3bd6d4663c
Rddrigo Cabrito <rodri.cabrito@gmail.com>
2024-01-21 22:37:26 +0000

    updating attributes to Properties and getters from the db

--- app/src/main/java/com/example/dailysync/Exercise.kt
+++ app/src/main/java/com/example/dailysync/Exercise.kt
@@ -1,13 +1,70 @@
 package com.example.dailysync
 
+import android.os.Parcelable
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.IgnoreExtraProperties
+import com.google.firebase.database.PropertyName
+import com.google.firebase.database.ValueEventListener
+import java.io.Serializable
 import java.time.Instant
+import kotlinx.parcelize.Parcelize
 
+@IgnoreExtraProperties
+@Parcelize
 data class Exercise(
-    val name: String,
-    val description: String = "",
-    val time: Long,
-    val averagePace: Float,
-    val distance: Float,
-    val image: Boolean,
-    val date: Instant
-)
+    @PropertyName("name") val name: String,
+    @PropertyName("description") val description: String = "",
+    @PropertyName("time") val time: Long,
+    @PropertyName("averagePace") val averagePace: Float,
+    @PropertyName("distance") val distance: Float,
+    @PropertyName("image") val image: Boolean,
+    @PropertyName("date") val date: Long  // Use Long to represent date as milliseconds
+) : Serializable, Parcelable {
+    constructor() : this(
+        "",
+        "",
+        0L,
+        0f,
+        0f,
+        false,
+        0L
+    )
+}
+
+class FirebaseExerciseDataManager {
+
+    private val databaseReference: DatabaseReference = FirebaseDatabase.getInstance().reference
+
+    fun getExercises(userId: String, category: Int, onComplete: (List<Exercise>) -> Unit) {
+        val categoryName = when (category) {
+            1 -> "Run"
+            2 -> "Walk"
+            else -> "Cycle"
+        }
+        val exerciseNodeReference = databaseReference.child("users").child(userId).child(categoryName)
+
+        exerciseNodeReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val exerciseList = mutableListOf<Exercise>()
+
+                Log.e("Depois", "Dentro do onDataChange do getExercises")
+                for (exerciseSnapshot in dataSnapshot.children) {
+                    val exercise = exerciseSnapshot.getValue(Exercise::class.java)
+                    exercise?.let {
+                        exerciseList.add(it)
+                    }
+                }
+
+                onComplete(exerciseList)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle errors here
+            }
+        })
+    }
+}
--- app/src/main/java/com/example/dailysync/Sleep.kt
+++ app/src/main/java/com/example/dailysync/Sleep.kt
@@ -1,12 +1,62 @@
 package com.example.dailysync
 
+import android.os.Parcelable
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.IgnoreExtraProperties
+import com.google.firebase.database.PropertyName
+import com.google.firebase.database.ValueEventListener
+import kotlinx.parcelize.Parcelize
+import java.io.Serializable
 import java.time.Instant
 
+@IgnoreExtraProperties
+@Parcelize
 data class Sleep(
-    val bedTimeHour: Int,
-    val bedTimeMin: Int,
-    val awakeTimeHour: Int,
-    val awakeTimeMin: Int,
-    val timeSlept: Pair<Int,Int>,
-    val date: Instant
-)
+    @PropertyName("bedTimeHour") val bedTimeHour: Int,
+    @PropertyName("bedTimeMin") val bedTimeMin: Int,
+    @PropertyName("awakeTimeHour") val awakeTimeHour: Int,
+    @PropertyName("awakeTimeMin") val awakeTimeMin: Int,
+    @PropertyName("timeSlept") val timeSlept: Pair<Int, Int>?,
+    @PropertyName("date") val date: Long
+): Serializable, Parcelable {
+    constructor() : this(
+        0,
+        0,
+        0,
+        0,
+        null,
+        0L
+    )
+}
+
+class FirebaseSleepDataManager {
+
+    private val databaseReference: DatabaseReference = FirebaseDatabase.getInstance().reference
+
+    fun getSleeps(userId: String, onComplete: (List<Sleep>) -> Unit) {
+        val sleepNodeReference = databaseReference.child("users").child(userId).child("Sleep")
+
+        sleepNodeReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val sleepList = mutableListOf<Sleep>()
+
+                for (sleepSnapshot in dataSnapshot.children) {
+                    val sleep = sleepSnapshot.getValue(Sleep::class.java)
+                    sleep?.let {
+                        sleepList.add(it)
+                    }
+                }
+
+                onComplete(sleepList)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle errors here
+            }
+        })
+    }
+}
+
